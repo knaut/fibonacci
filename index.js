8805b0058cf48b8a62306a694aef94bca544f0ac
knaut <daniel@smallinvisiblemachines.com>
2020-05-06 20:34:52 -0700

    basic recursive solution, with issues

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // print out 'n' iterations
 // e.g. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144…
 
+const ITERATION_LIMIT = 100
 
-function recursive(first, last, limit) {
-	const addLast = first + last
+function recursive(last, next, limit) {
+	
+	const iteration = last + next
 
-	console.log({first, addLast})
+	if (iteration <= limit) {
 
-	if (addLast <= limit) {
-		return recursive(last, addLast, limit)
+		console.log(iteration)
+
+		return recursive(next, iteration, limit)
 	}
 }
 
-console.log(recursive(0, 1, 10))
+console.log(recursive(0, 1, ITERATION_LIMIT))
