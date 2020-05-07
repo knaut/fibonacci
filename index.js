5cca34fdb3281131bc43363e20709fe5fdff8b15
knaut <daniel@smallinvisiblemachines.com>
2020-05-06 20:44:36 -0700

    cleanup, but we want to use ITERATION_LIMIT as a number of iteration "times" for "n"

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 // e.g. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144…
 
 const ITERATION_LIMIT = 100
+let iterationTimes = 0
 
 function recursive(last, next, limit) {
-	
+	iterationTimes = iterationTimes + 1
+
 	const iteration = last + next
+
+	console.log({iterationTimes, iteration})
 
 	if (iteration <= limit) {
 
-		console.log(iteration)
-
 		return recursive(next, iteration, limit)
+		
 	}
 }
 
