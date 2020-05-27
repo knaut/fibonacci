9c1a2b46bf055556c41d888ad55e320299a75d14
knaut <daniel@smallinvisiblemachines.com>
2020-05-26 19:08:03 -0700

    change tabs to spaces

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const ITERATION_LIMIT = 100
 let iterationTimes = 0
 
 function recursive(last, next, limit) {
-	iterationTimes = iterationTimes + 1
+  iterationTimes = iterationTimes + 1
 
-	const iteration = last + next
+  const iteration = last + next
 
-	console.log({iterationTimes, iteration})
+  console.log({iterationTimes, iteration})
 
-	if (iterationTimes <= limit) {
+  if (iterationTimes <= limit) {
 
-		return recursive(next, iteration, limit)
+    return recursive(next, iteration, limit)
 
-	}
+  }
 }
 
 console.log(recursive(0, 1, ITERATION_LIMIT))
